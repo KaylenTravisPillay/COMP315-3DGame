e71436adeaf3063294c8c1b892bcef2bde5d6a0a
ktpso <ktpsolutions@outlook.com>
2017-01-29 13:30:15 +0200

    Uploading first cpp proj

---
+++ test/proj1.cpp
@@ -0,0 +1,30 @@
+/************************************************************************
+** File:    proj1.cpp
+** Project: Testing IDE setup, Sem1 2017
+** Author:  Kaylen Travis Pillay
+** Date:    29 January 2017
+** Section: Team_Alpha BC COMP315 Group project
+** E-mail:  kaylentp@ktpsolutions.co.za
+**
+**  #This file contains the main driver program for the Test project#
+** This program tests that each team members IDE and complieris setup
+** properly by displaying the member of the team in a command line app
+*************************************************************************/
+#include <iostream>
+#include <string>
+
+//The main method of the test project
+int main()
+{
+    std::cout << std::endl;
+    std::cout << "Team_Alpha BC Members:" << std::endl;
+    std::cout << "COMP315 Group project \n" << std::endl;
+    std::cout << "- Kimyran " << std::endl;
+    std::cout << "- Mpilo " << std::endl;
+    std::cout << "- Theo " << std::endl;
+    std::cout << "- Tuh " << std::endl;
+    std::cout << "- Kaylen " << std::endl;
+    std::cout << std::endl;
+
+    return 0;
+}
